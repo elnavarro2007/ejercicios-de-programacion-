8c0f85943c5405aa326d632f14adb2039148c203
elnavarro2007 <diegonavarrofernandez07@gmail.com>
2025-10-22 17:15:59 +0200

    ejercicios unidad 3

---
+++ programacion actividades/ejerciciosU3/src/EJ1.java
@@ -0,0 +1,14 @@
+import java.lang.annotation.Repeatable;
+
+import java.util.Scanner;
+
+
+public class EJ1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce las ptas que quieras para pasarlos a euros");
+        float ptas= sc.nextFloat();
+        double euros = ptas/166.386;
+        System.out.printf("hay esta cantidad de euros %.3f\n ",euros );
+    }
+}
