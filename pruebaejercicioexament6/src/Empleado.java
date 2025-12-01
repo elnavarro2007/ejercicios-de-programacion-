11f362a6cb0433603c6c1c8e2d692d1a26d26392
Diego Navarro <diegonavarrofernandez07@gmail.com>
2025-12-01 20:55:48 +0100

    Add files via upload

---
+++ pruebaejercicioexament6/src/Empleado.java
@@ -0,0 +1,82 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Empleado {
+
+    String nombre;
+    String apellido;
+    LocalDate fechaDeContratacion;
+    String DNI;
+    Double salario;
+    String puesto;
+
+    public String getNombre() {
+        return nombre;
+
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFechaDeContratacion() {
+        return fechaDeContratacion;
+    }
+
+    public void setFechaDeContratacion(LocalDate fechaDeContratacion) {
+        fechaDeContratacion = fechaDeContratacion;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public void trabajar() {
+        System.out.println("estoy trabajando como " + puesto);
+    }
+
+    public String toString() {
+        return "Nombre :" + nombre +
+                "apellido :" + apellido +
+                "Fecha contratacion :" + fechaDeContratacion +
+                "DNI :" + DNI +
+                "Salario :" + salario +
+                "Puesto :" + puesto;
+
+
+    }
+
+    public Integer antiguedad() {
+
+        return Period.between(fechaDeContratacion,LocalDate.now()).getDays();
+    }
+}
+
---
+++ pruebaejercicioexament6/src/Empresa.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Empresa {
+
+  ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+
+    public ArrayList<Empleado> getEmpleado() {
+        return empleados;
+    }
+
+    public Empresa(ArrayList<Empleado> empleado) {
+        this.empleados = empleado;
+
+    }
+    public void devolverempleados() {
+        for (Empleado e: empleados) {
+            System.out.println(e.toString());
+        }
+    }
+    public Double salariopromedio (){
+        Double sumaSalario = 0.0;
+        for (Empleado e:empleados){
+            sumaSalario = sumaSalario+e.getSalario();
+        }
+        return sumaSalario;
+
+    }
+    public LocalDate antiguedad (){
+        for (Empleado e: empleados){
+            
+        }
+
+
+    }
+
+
+
+
+}
