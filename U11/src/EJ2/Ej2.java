8d95b29aa5b443b95f0cc1d1786dba0450ad6290
Diego Navarro <diegonavarrofernandez07@gmail.com>
2026-02-16 19:24:54 +0000

    Add files via upload

--- U11/src/EJ2/Ej2.java
+++ U11/src/EJ2/Ej2.java
@@ -1,5 +1,31 @@
 package EJ2;
 
-public class Ej2 {
 
+//2. Crea un programa que elija al azar 3 números distintos entre 0 y 9.
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Random;
+import java.util.regex.Matcher;
+
+public class Ej2 {
+    public static void main(String[] args) {
+        Random numero = new Random();
+
+
+
+
+        List<Integer> lista = new ArrayList<>();
+       
+        for (int i= 0;i<3;i++){
+            //System.out.println(numero.nextInt(9));
+            lista.add((numero.nextInt(9)));
+            System.out.print(lista.get(i) + " ");
+
+
+
+        }
+    }
 }
